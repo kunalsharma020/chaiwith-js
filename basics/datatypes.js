921b049ac8071326e5a7dd223ad1e07e858e1f51
Kunal Sharma <your_email@example.com>
2026-05-30 16:57:36 +0530

    change

---
+++ basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict" // traeat all code as modern 
+
+// alert("hello") // we are using nodejs not browser so alert is not defined
+
+
+//  primitive data types
+ let name="kunal"
+ let age=20
+ let isLoggedIn=false
+//   null -> empty value it can be assign 
+//  undefined -> value is not assigned
+//  symbol -> unique identifier
+
+// object data types
+console.log(typeof name) // string
+console.log(typeof age) // number
+console.log(typeof isLoggedIn) // boolean
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+console.log(typeof Symbol("id")) // symbol
---
+++ basics/text.js
@@ -0,0 +1,17 @@
+const accountId=14454
+let accountEmail="kunal@gmail.com"
+var accountPassword="12345"
+accountCity="Delhi"
+let accountState;
+// accountId=12345 not allowed
+
+accountEmail="hc@gmail.com"
+accountPassword="212121"
+accountCity="Banglore"
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/*
+prefer not to use var because of issue in function and block scope 
+
+*/
