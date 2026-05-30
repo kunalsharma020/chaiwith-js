7aca16f65bc00ab953126380605cce401a2277f8
Kunal Sharma <your_email@example.com>
2026-05-30 17:28:04 +0530

    change

---
+++ basics/conversion.js
@@ -0,0 +1,27 @@
+// // let score="33abs" // string
+// let score =null
+// console.log( score) // string
+
+// // console.log(typeof score  ) 
+
+// let valInNumber=Number(score) // convert to number
+// console.log(typeof valInNumber)
+// console.log(valInNumber) // NaN -> Not a Number
+
+//  "33" -> 33
+//  "33abs" -> NaN
+//  null -> 0
+//  undefined -> NaN
+//  true -> 1   
+//  false -> 0
+
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn) // true
+
+//  1-> true
+// 0 -> false
+// "" -> false
+// "kunal" -> true
+// null -> false
+// undefined -> false
---
+++ basics/operations.js
@@ -0,0 +1,31 @@
+// let value=3 
+// let negVal=-value
+// console.log(value) // 3
+// console.log(negVal) // -3
+
+
+// console.log(-(-3)) // 3
+// console.log(-(-(-3))) // -3
+// console.log(2+2)    // 4
+// console.log(2-2)    // 0
+// console.log(2*2)    // 4
+// console.log(2/2)    // 1
+// console.log(2%2)    // 0
+
+// let str1="hello"
+// let str2="world"
+// console.log(str1+str2) // helloworld
+// console.log(str1+" "+str2) // hello world
+
+// console.log("1"+2) // 12
+// console.log(1+"2") // 12
+// console.log("1"+2+3) // 123
+// console.log(1+"2"+3) // 123
+// console.log(1+2+"3") // 33
+
+let num1, num2, num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++ // 101
+console.log(gameCounter) // 101
